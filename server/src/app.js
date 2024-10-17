bda4fd27fbf8570686902b3db98e6ea85cce1bc3
Pentapati Seshagiri <Pentapati.Seshagiri@ivycomptech.com>
2024-10-17 12:13:03 +0530

    added promises

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
-app.use(cors());
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(planetsRouter);
 module.export = app;
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,6 +1,7 @@
 
-const planets = require('../../models/planets.model');
+const { planets } = require('../../models/planets.model');
 function getAllPlanets(req, res) {
+    console.log("seshu", planets)
     return res.status(200).json(planets)
 }
 
